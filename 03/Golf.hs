c4b53f243ee3044c497d177c3024450ce8a66853
Evan Turner <ev@evturn.com>
2018-01-30 21:37:12 -0500

    Complete exercise 2 from HW3

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -3,6 +3,8 @@ module Golf where
 import           Data.Bool
 import           Data.List
 
+-----------------------------------------------------------------------------
+-- 1.
 skips :: [a] -> [[a]]
 skips = filter (not . null) . runSkip
   where
@@ -11,3 +13,13 @@ skips = filter (not . null) . runSkip
     nth xs     = foldr (\a b -> [a] : b) [] (drop 2 xs)
     snds xs    = foldr fsnd [] (zip xs [1..])
     fsnd x y   = bool y (fst x : y) (even $ snd x)
+
+-----------------------------------------------------------------------------
+-- 2.
+localMaxima :: [Int] -> [Int]
+localMaxima xs = sort $ go xs []
+  where
+    go [] ys         = ys
+    go (x:[]) ys     = ys
+    go (x:y:[]) ys   = ys
+    go (x:y:z:xs) ys = go (y:z:xs) (bool ys (y:ys) (y > x && y > z))
